0a4be8817142339a0791a1e8d83629ce202b794a
anyaprian <72358102+anyaprian@users.noreply.github.com>
2023-04-24 23:36:05 +1000

    Update app.py

--- app.py
+++ app.py
@@ -1,6 +1,6 @@
 import streamlit as st
 from transformers import BertTokenizerFast, BertForTokenClassification
-import torch
+import numpy as np
 
 @st.cache_data()
 def get_model():
@@ -15,28 +15,28 @@ user_input = st.text_area('Text')
 button = st.button("Find Entities")
 
 ids_to_labels = {0: 'O',
- 1: 'B-HOTEL_NAME',
- 2: 'I-HOTEL_NAME',
- 3: 'B-PROPERTY_PHONE',
- 4: 'I-PROPERTY_PHONE',
- 5: 'B-PIN_CODE',
- 6: 'B-RESERVATION_NUMBER',
- 7: 'B-CHECK_IN_DATE_TIME',
- 8: 'I-CHECK_IN_DATE_TIME',
- 9: 'B-CHECK_OUT_DATE_TIME',
- 10: 'I-CHECK_OUT_DATE_TIME',
- 11: 'B-LOCATION',
- 12: 'I-LOCATION',
- 13: 'B-MAX_CAPACITY',
- 14: 'I-MAX_CAPACITY',
- 15: 'B-GUEST_NAME',
- 16: 'I-GUEST_NAME',
- 17: 'B-TOTAL_PRICE',
- 18: 'I-TOTAL_PRICE',
- 19: 'B-NUMBER_OF_GUESTS',
- 20: 'I-NUMBER_OF_GUESTS',
- 21: 'B-CANCELLATION_FEE',
- 22: 'I-CANCELLATION_FEE'}
+                 1: 'B-HOTEL_NAME',
+                 2: 'I-HOTEL_NAME',
+                 3: 'B-PROPERTY_PHONE',
+                 4: 'I-PROPERTY_PHONE',
+                 5: 'B-PIN_CODE',
+                 6: 'B-RESERVATION_NUMBER',
+                 7: 'B-CHECK_IN_DATE_TIME',
+                 8: 'I-CHECK_IN_DATE_TIME',
+                 9: 'B-CHECK_OUT_DATE_TIME',
+                 10: 'I-CHECK_OUT_DATE_TIME',
+                 11: 'B-LOCATION',
+                 12: 'I-LOCATION',
+                 13: 'B-MAX_CAPACITY',
+                 14: 'I-MAX_CAPACITY',
+                 15: 'B-GUEST_NAME',
+                 16: 'I-GUEST_NAME',
+                 17: 'B-TOTAL_PRICE',
+                 18: 'I-TOTAL_PRICE',
+                 19: 'B-NUMBER_OF_GUESTS',
+                 20: 'I-NUMBER_OF_GUESTS',
+                 21: 'B-CANCELLATION_FEE',
+                 22: 'I-CANCELLATION_FEE'}
 
 def bio_to_dict(tokens, tags):
     """Convert BIO tagged text to a dictionary of entities and their spans"""
@@ -77,30 +77,31 @@ if user_input and button :
                         max_length=512,
                         return_tensors="pt")
 
+    move to gpu
     ids = inputs["input_ids"]
     mask = inputs["attention_mask"]
     # forward pass
     outputs = model(ids, attention_mask=mask)
     logits = outputs[0]
 
-#     active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
-#     flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size*seq_len,) - predictions at the token level
+    active_logits = logits.reshape(-1, model.num_labels) 
+    flattened_predictions = np.argmax(active_logits.detach().numpy(), axis=1)
 
-#     tokens = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
-#     token_predictions = [ids_to_labels[i] for i in flattened_predictions.numpy()]
-#     wp_preds = list(zip(tokens, token_predictions)) # list of tuples. Each tuple = (wordpiece, prediction)
+    tokens = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
+    token_predictions = [ids_to_labels[i] for i in flattened_predictions.cpu().numpy()]
+    wp_preds = list(zip(tokens, token_predictions)) # list of tuples. Each tuple = (wordpiece, prediction)
 
-#     tags = []
-#     for token_pred, mapping in zip(wp_preds, inputs["offset_mapping"].squeeze().tolist()):
-#       if mapping[0] == 0 and mapping[1] != 0:
-#         tags.append(token_pred[1])
-#       else:
-#         continue
-#     tokens = user_input.split()
+    tags = []
+    for token_pred, mapping in zip(wp_preds, inputs["offset_mapping"].squeeze().tolist()):
+      if mapping[0] == 0 and mapping[1] != 0:
+        tags.append(token_pred[1])
+      else:
+        continue
+    tokens = user_input.split()
     st.write(user_input)
     del (
-        model,
-        tokenizer
-#         outputs,
-#         logits
-    )
+            model,
+            tokenizer,
+            outputs,
+            logits
+        )
