14626a0de87127d9deabe6f98684ebee6a7172a6
anyaprian <72358102+anyaprian@users.noreply.github.com>
2023-04-24 23:19:57 +1000

    Update app.py

--- app.py
+++ app.py
@@ -77,14 +77,14 @@ if user_input and button :
                         max_length=512,
                         return_tensors="pt")
 
-#     ids = inputs["input_ids"]
-#     mask = inputs["attention_mask"]
-#     # forward pass
-#     outputs = model(ids, attention_mask=mask)
-#     logits = outputs[0]
+    ids = inputs["input_ids"]
+    mask = inputs["attention_mask"]
+    # forward pass
+    outputs = model(ids, attention_mask=mask)
+    logits = outputs[0]
 
-#     active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
-#     flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size*seq_len,) - predictions at the token level
+    active_logits = logits.view(-1, model.num_labels) # shape (batch_size * seq_len, num_labels)
+    flattened_predictions = torch.argmax(active_logits, axis=1) # shape (batch_size*seq_len,) - predictions at the token level
 
 #     tokens = tokenizer.convert_ids_to_tokens(ids.squeeze().tolist())
 #     token_predictions = [ids_to_labels[i] for i in flattened_predictions.numpy()]
